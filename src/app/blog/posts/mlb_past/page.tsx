7122e8afad6c1359d1f186104f1cea38e19be41d
DamonCharlesRoberts <damon.charles.roberts@gmail.com>
2025-02-22 19:02:42 -0500

    Blog post ready.

--- src/app/blog/posts/mlb_past/page.tsx
+++ src/app/blog/posts/mlb_past/page.tsx
@@ -63,6 +63,49 @@ export default function Post() {
     if (scriptIndex === 2) setIsOpen2(!isOpen2);
     if (scriptIndex === 3) setIsOpen3(!isOpen3);
   };
+  const [seasons, setSeasons] = useState(2019);
+  const [model, setModel] = useState("Simple");
+  // Function to handle year change
+  const handleSeasonChange = (event) => {
+    setSeasons(event.target.value);
+  };
+  // Function to handle model change
+  const handleModelChange = (event) => {
+    setModel(event.target.value);
+  };
+  const plotsData = {
+    2019: {
+      "Simple": "/2019_btl_estimates.html"
+      ,"Home-field adv.": "/2019_home_estimates.html"
+      ,"Ordered outcome": "/2019_mag_estimates.html"
+    },
+    2020: {
+      "Simple": "/2020_btl_estimates.html"
+      ,"Home-field adv.": "/2020_home_estimates.html"
+      ,"Ordered outcome": "/2020_mag_estimates.html"
+    },
+    2021: {
+      "Simple": "/2021_btl_estimates.html"
+      ,"Home-field adv.": "/2021_home_estimates.html"
+      ,"Ordered outcome": "/2021_mag_estimates.html"
+    },
+    2022: {
+      "Simple": "/2022_btl_estimates.html"
+      ,"Home-field adv.": "/2022_home_estimates.html"
+      ,"Ordered outcome": "/2022_mag_estimates.html"
+    },
+    2023: {
+      "Simple": "/2023_btl_estimates.html"
+      ,"Home-field adv.": "/2023_home_estimates.html"
+      ,"Ordered outcome": "/2023_mag_estimates.html"
+    },
+    2024: {
+      "Simple": "/2024_btl_estimates.html"
+      ,"Home-field adv.": "/2024_home_estimates.html"
+      ,"Ordered outcome": "/2024_mag_estimates.html"
+    },
+  };
+  const plot = plotsData[seasons]?.[model];
   return (
     <section>
       <h1 className="mb-8 text-center text-2xl font-semibold tracking-tighter">
@@ -92,7 +135,7 @@ export default function Post() {
       >
         The task of sorting this out seems relatively complicated at first blush.
         And it kind of is, thankfully we have a class of statistical models that
-        do not force us to deliberate on a committee. Instead, we can estimate each
+        do not force us to deliberate in a committee. Instead, we can estimate each
         team's latent (latent meaning that we cannot directly measure it easily
         since not every team plays against one another) ability based on who they win
         against and who they lose against.
@@ -102,11 +145,15 @@ export default function Post() {
         style={{ opacity: getOpacity(1), transition: "opacity 0.2s ease-out" }}
       >
         A class of statistical models allow us to do just that: Paired comparisons.
-        A specific form of these models is called the Bradley Terry model.
+        One type of these models is called
+        the <a href="https://www.jstor.org/stable/2334029?origin=crossref">
+          <u>Bradley-Terry model</u>
+        </a>.
       </p>
       <br></br>
       <MathJax.Provider>
-        <MathJax.Node formula={'y_i = logit^{-1}(\\alpha_{a}-\\alpha_{h})'} />
+        <MathJax.Node formula={"y_i = logit^{-1}(\\alpha_{a}-\\alpha_{h})"} />
+        <MathJax.Node formula={"\\alpha \\sim \\mathcal{N}(0,1)"} />
       </MathJax.Provider>
       <br></br>
       <p
@@ -128,7 +175,7 @@ export default function Post() {
       <br></br>
       <div>
         <button onClick={() => toggleCollapse(1)}>
-          {isOpen1 ? 'Hide Code' : 'Show STAN code for BT'}
+          {isOpen1 ? 'Hide code ↑' : 'Show STAN code ↓'}
         </button>
         {isOpen1 && (
           <pre>
@@ -148,11 +195,13 @@ export default function Post() {
       <br></br>
       <MathJax.Provider>
         <MathJax.Node formula={'y_i = logit^{-1}(\\alpha_{a}-\\alpha_{h} + \\gamma)'} />
+        <MathJax.Node formula={"\\alpha \\sim \\mathcal{N}(0,1)"} />
+        <MathJax.Node formula={"\\gamma \\sim \\mathcal{N}(0,1)"} />
       </MathJax.Provider>
       <br></br>
       <div>
         <button onClick={() => toggleCollapse(2)}>
-          {isOpen2 ? 'Hide Code' : 'Show STAN code for Home adv.'}
+          {isOpen2 ? 'Hide code ↑' : 'Show STAN code ↓'}
         </button>
         {isOpen2 && (
           <pre>
@@ -165,8 +214,11 @@ export default function Post() {
         style={{ opacity: getOpacity(3), transition: "opacity 0.2s ease-out" }}
       >
         <MathJax.Provider><span>
-          Finally, I run an extension of the Bradley-Terry model which is referred
-          to as the Davidson model which allows for ties and an ordered set of
+          Finally, I run an extension of the Bradley-Terry model which is sometimes
+          referred to as 
+          the <a href="https://www.tandfonline.com/doi/abs/10.1080/01621459.1970.10481082">
+            <u>Davidson model</u>
+          </a> which allows for ties and an ordered set of
           outcomes. That is, rather than simply predicting whether or not a team
           won or not, now my model is supposed to predict the magnitude of the win.
           That is, in this final model, <MathJax.Node inline formula={'y_i '}/> is
@@ -183,11 +235,13 @@ export default function Post() {
       <br></br>
       <MathJax.Provider>
         <MathJax.Node formula={'y_i = ordered\\_logit^{-1}(\\alpha_{a}-\\alpha_{h} + \\gamma)'} />
+        <MathJax.Node formula={"\\alpha \\sim \\mathcal{N}(0,1)"} />
+        <MathJax.Node formula={"\\gamma \\sim \\mathcal{N}(0,1)"} />
       </MathJax.Provider>
       <br></br>
       <div>
         <button onClick={() => toggleCollapse(3)}>
-          {isOpen3 ? 'Hide Code' : 'Show STAN code for Davidson'}
+          {isOpen3 ? 'Hide code ↑' : 'Show STAN code ↓'}
         </button>
         {isOpen3 && (
           <pre>
@@ -197,14 +251,112 @@ export default function Post() {
       </div>
       <br></br>
       <div>
-      <iframe
-        src="/2019_estimates.html"
-        width="100%"
-        height="500px"
-        title="Plotly Chart"
-        frameBorder="0"
-      ></iframe>
-    </div>
+      <br></br>
+      <p
+        style={{ opacity: getOpacity(4), transition: "opacity 0.2s ease-out" }}
+      >
+      I pulled the boxscores for the 2019, 2020, 2021, 2022, 2023, and 2024 regular seasons
+      from the MLB API. With these data, I fit these three models using 
+      <code> cmdstanpy</code>. I fit these models and retain the last 2000 simulations
+      (or draws) of the estimated rankings.
+      The full repository containing all of this can be found
+      on my <a href="https://github.com/DamonCharlesRoberts/mlb_pred">
+            <u>GitHub</u>
+          </a>.
+      </p>
+      <p
+        style={{ opacity: getOpacity(5), transition: "opacity 0.2s ease-out" }}
+      >
+      The results of these models are in the plots below. The dot represents
+      the median ranking for each team that the model has estimated. The bars
+      reflect the range of the model's uncertainty about the ranking of the
+      team. This range reflects that the team's rank is expected to fall
+      within this range 95% of the time.
+      </p>
+      <br></br>
+      <br></br>
+      <select 
+          value={seasons} onChange={handleSeasonChange}
+          style={{
+              padding: '10px',
+              fontSize: '16px',
+              borderRadius: '5px',
+              border: '1px solid #ccc',
+              width: '200px',
+              color: "#ffffff",
+              background: "#000000"
+            }}
+      >
+        {Object.keys(plotsData).map((seasonOption) => (
+          <option key={seasonOption} value={seasonOption}>
+            {seasonOption}
+          </option>
+        ))}
+      </select>
+      <select 
+          value={model} onChange={handleModelChange}
+          style={{
+              padding: '10px',
+              fontSize: '16px',
+              borderRadius: '5px',
+              border: '1px solid #ccc',
+              width: '200px',
+              color: "#ffffff",
+              background: "#000000"
+            }}
+      >
+        {Object.keys(plotsData[seasons]).map((modelOption) => (
+          <option key={modelOption} value={modelOption}>
+            {modelOption}
+          </option>
+        ))}
+      </select>
+      </div>
+      <div>
+        {plot ? (
+          <iframe
+              src={plot}
+              width="150%"
+              height="750px"
+              title={"Estimated rank"}
+          ></iframe>
+        ) : (
+          <p>Select a valid season and model to see the content.</p>
+        )}
+      </div>
+      <br></br>
+      <br></br>
+      <p
+        style={{ opacity: getOpacity(6), transition: "opacity 0.2s ease-out" }}
+      >
+      There are a few tweaks I could make to these models:
+      </p><br></br>
+      <p
+        style={{ opacity: getOpacity(7), transition: "opacity 0.2s ease-out" }}
+      >
+          - Play around with the priors for the parameters a bit more.
+          Though from testing, I don't think they will make too much of a
+          difference here.
+      </p>
+      <p
+        style={{ opacity: getOpacity(8), transition: "opacity 0.2s ease-out" }}
+      >
+          - I could do things like a hierarchical model where I model
+          player ability and sum the ability of the players for each
+          team. One issue with this approach is that the players for
+          each team tends to remain relatively stable over time. While
+          pitching may change, the rotation of pitchers tends to remain
+          somewhat stable. So, it may be something to try, but my
+          <i> a priori</i> expectation is that it won't help the model
+          much.
+      </p>
+      <p
+        style={{ opacity: getOpacity(9), transition: "opacity 0.2s ease-out" }}
+      >
+          - There may be more that I am not thinking of, but Spring Training
+          is underway and I am just too excited to sit on this project too
+          much longer!
+      </p>
     </section>
   );
 }
